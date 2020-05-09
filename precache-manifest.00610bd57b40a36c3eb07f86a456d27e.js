3c5a44e22053bb465f2d6fccf8cee940240eedb9
Hammad Khan <h4hammad@live.com>
2020-05-10 02:23:12 +0400

    Updates

---
+++ precache-manifest.00610bd57b40a36c3eb07f86a456d27e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee8211f0a687f826fa645c929e80071",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f7e6abaf881819ecb9c7",
+    "url": "/monsters-rolodex/static/css/main.00b53fc0.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e6abaf881819ecb9c7",
+    "url": "/monsters-rolodex/static/js/main.8a8434f3.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
